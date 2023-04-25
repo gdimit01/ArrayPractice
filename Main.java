72b1ce8e57a5df55c7fab5d4f852de17aeacbf7f
Georgi Dimitrov <gd353@kent.ac.uk>
2023-04-25 16:45:38 +0100

    fixed problem with conditionals

--- Main.java
+++ Main.java
@@ -1,32 +1,27 @@
 import java.util.*;
+
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("How many days' temp?");
         int numDays = scanner.nextInt(); // taking the number of days based on this variable
-        //initialize array
-        int[] temps = new int[numDays];
-        // record temps and find the average
-        int sum = 0;
-        for (int i = 0; i <=numDays; i++) {
-            System.out.println("Day " + (i+1) + "'s high temp:");
-            //each time user enters temp, it is saved into the temp[i] array
+        int[] temps = new int[numDays]; //initialize array
+        int sum = 0; // record temps and find the average
+        for (int i = 0; i < numDays; i++) { // fix the loop condition
+            System.out.println("Day " + (i + 1) + "'s high temp:");
             temps[i] = scanner.nextInt();
-            sum += temps[i]; //based on that we calculate the sum of temp
+            sum += temps[i]; //each time user enters temp, it is saved into the temp[i] array
         }
-        double average = sum / numDays;
-        //count days above average
+        double average = sum / (double) numDays; // convert to double to avoid integer division
         int above = 0;
-        for (int i = 0; i <temps.length; i++) {
-            if (temps[i] > average)//if temp is above average
-            {
+        for (int i = 0; i < temps.length; i++) {
+            if (temps[i] > average) {
                 above++;
             }
-            
         }
 
         System.out.println();
         System.out.println("Average temp is " + average);
         System.out.println(above + " days' temps are above average");
-    }    
+    }
 }
